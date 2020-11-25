e1723aacce372d00a2fd60cbf7cc8c4c3d1e7b3c
ArnauAdan <72872055+ArnauAdan@users.noreply.github.com>
2020-11-25 11:32:37 +0100

    Update Seller.java

--- LAB_3/OnlineStore/src/onlinestore/Seller.java
+++ LAB_3/OnlineStore/src/onlinestore/Seller.java
@@ -23,7 +23,10 @@ public class Seller extends User {
         accountNumber = a;
     }
     
+    //HE afegit treure i posar a les llistes, res més
     public void sell(Item i){
+        soldItem.add(i);
+        availableItems.remove(i);
         System.out.println(this.getName() + "sold" + i.getName() + "and" + i.calculateProfit() + "euros are deposited to account" + accountNumber);
     }
     
@@ -31,6 +34,13 @@ public class Seller extends User {
         availableItems.add(i);
     }
     
+    //El que entenia d'aquest mètode és que et deia si era correcta el preu i he posat
+    //Tot i que veient el teu també imprimiria missatge no sé que en penses
+    
+    /* public boolean deposit(double price){
+            return soldItem.getLast().getPrice() == price;
+       }*/
+   
     private boolean deposit(double price){
         System.out.println("User" + this.getName() + "wishes to deposit" + price + "directly to the banck account" + accountNumber);
         return true;
